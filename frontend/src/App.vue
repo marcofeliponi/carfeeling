e5116d464e2ed42d4de8c6c59b74fef2dc094a37
Marco <marco.feliponi@outlook.com>
2024-09-10 21:26:16 -0300

    initial navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import NavBar from './components/NavBar.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="app-navbar">
+    <NavBar />
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style scoped lang="scss">
+.app-navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 100;
 }
 </style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="navbar">
+        <div class="app-logo">
+            <Icon color="white" size="48">
+                <CarOutline />
+            </Icon>
+        </div>
+        <div class="hamburger-button">
+            <Icon class="menu-icon" color="white" size="40" @click="openMenu">
+                <MenuOutline>
+                </MenuOutline>
+            </Icon>
+        </div>
+    </div>
+</template>
+
+<script>
+import { CarOutline, MenuOutline } from '@vicons/ionicons5'
+ import { Icon } from '@vicons/utils'
+
+export default {
+    name: 'NavBar',
+
+    components: {
+        CarOutline,
+        Icon,
+        MenuOutline
+    },
+
+    methods: {
+        openMenu() {
+            console.log('menu opened')
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.navbar {
+    width: 100%;
+    height: 100%;
+    background-color: var(--primary-color);
+    display: flex;
+    align-items: center;
+
+    .app-logo {
+        width: 50%;
+        height: 50%;
+        display: flex;
+        justify-content: end;
+    }
+
+    .hamburger-button {
+        width: 50%;
+        height: 50%;
+        display: flex;
+        justify-content: end;
+        margin-right: 10px;
+
+        .menu-icon {
+            cursor: pointer;
+        }
+    }
+
+}
+</style>
