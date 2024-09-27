2102e5c113ce784d781378cef1fd7c048060aa1f
Marco <marco.feliponi@outlook.com>
2024-09-26 22:11:04 -0300

    connect to firestore

--- backend/services/service.py
+++ backend/services/service.py
@@ -1,8 +1,14 @@
 import requests
 import os
 from flask import Flask, jsonify
+import os
+from firebase_admin import firestore, initialize_app
+
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
+
+initialize_app()
+db = firestore.client()
 
 @app.route("/")
 def home():
