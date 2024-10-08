906d509ecc34d6c2e75e5b8dd8b34116c74c8391
Marco <marco.feliponi@outlook.com>
2024-10-08 20:52:53 -0300

    implements flask cors

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 import os
 from routes import routes
 
@@ -8,6 +9,7 @@ port = int(os.environ.get('PORT', 5000))
 os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
 
 app.register_blueprint(routes.bp)
+CORS(app, origins=['http://localhost:5173'])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)
