81633661eb6894298ebab7c7f2a137ae2f59af56
Marco <marco.feliponi@outlook.com>
2024-10-16 17:37:25 -0300

    implements flask env at backend

--- backend/run.py
+++ backend/run.py
@@ -4,12 +4,22 @@ from flask_cors import CORS
 import os
 from routes import routes
 
+env = os.environ.get('FLASK_ENV', 'development')
+
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
-os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
+
+if env != 'production':
+    port = int(os.environ.get('PORT', 5000))
+    os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
+else:
+    port = int(os.environ.get('PORT', 8080))
 
 app.register_blueprint(routes.bp)
 CORS(app, origins=['http://localhost:5173'])
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)
