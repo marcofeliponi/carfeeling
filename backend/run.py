e4cdef7cc779ccf3547202b54aebec952e50ea86
Marco <marco.feliponi@outlook.com>
2024-10-16 19:49:59 -0300

    adjust backend to production server

--- backend/run.py
+++ backend/run.py
@@ -8,13 +8,11 @@ app = Flask(__name__)
 env = os.environ.get('FLASK_ENV', 'development')
 
 if env != 'production':
-    port = int(os.environ.get('PORT', 5000))
     os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
-else:
-    port = int(os.environ.get('PORT', 8080))
 
 app.register_blueprint(routes.bp)
 CORS(app, origins=['http://localhost:5173'])
 
 if __name__ == "__main__":
-    app.run(debug=(env != 'production'), host="0.0.0.0", port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
