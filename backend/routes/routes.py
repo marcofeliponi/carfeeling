b40757705844bd7031ec44b14816284a4a97a973
Marco <marco.feliponi@outlook.com>
2024-10-16 18:15:16 -0300

    just logs to debug

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -3,6 +3,10 @@ from services import services
 
 bp = Blueprint('routes', __name__)
 
+@bp.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 @bp.route('/api/cars')
 def get_cars():
     return services.get_cars_service()
