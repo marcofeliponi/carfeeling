02bc108e67db1f910a820981a4f279c08fe6102a
Marco <marco.feliponi@outlook.com>
2024-10-21 20:37:00 -0300

    frontend envs

--- frontend/src/views/CarConsult.vue
+++ frontend/src/views/CarConsult.vue
@@ -130,9 +130,7 @@ export default {
 
         async getBrands() {
             try {
-                console.log('envs', { meta: import.meta.env.VITE_API_URL, process: process.env.VITE_API_URL });
-                
-                const response = await axios.get(`${import.meta.env.VITE_API_URL ?? process.env.VITE_API_URL}/brands`);
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/brands`);
 
                 this.carBrands = response.data.brands;
             } catch (error) {
@@ -142,7 +140,7 @@ export default {
 
         async getCars() {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_API_URL ?? process.env.VITE_API_URL}/cars`);
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/cars`);
 
                 this.cars = response.data.cars;
             } catch (error) {
