6643164567667a6a966edac8f46cf5c2a504a5e4
Marco <marco.feliponi@outlook.com>
2024-10-27 19:42:23 -0300

    removes scrape execution from run.py

--- backend/run.py
+++ backend/run.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from routes import routes
-from scripts import web_scraping
 
 app = Flask(__name__)
 
@@ -13,8 +12,6 @@ if env != 'production':
 
 app.register_blueprint(routes.bp)
 CORS(app, resources={r"/*": {"origins": ['http://localhost:5173', 'https://carfeeling-33182721445.southamerica-east1.run.app']}})
-
-# web_scraping.run()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
