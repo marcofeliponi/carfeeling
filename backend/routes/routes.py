1cdd72ed552aa11ba983e2fccfcd97fac1bc4705
Marco <marco.feliponi@outlook.com>
2024-11-03 16:31:43 -0300

    route to scrape data

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -1,26 +1,33 @@
 from flask import Blueprint, request
 from services import services
+from scripts import web_scraping
+import asyncio
 
 bp = Blueprint('routes', __name__)
 
-@bp.route('/')
+@bp.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
 
-@bp.route('/api/cars')
+@bp.route('/api/cars', methods=['GET'])
 def get_cars():
     return services.get_cars_service()
 
-@bp.route('/api/brands')
+@bp.route('/api/brands', methods=['GET'])
 def get_brands():
     return services.get_brands_service()
 
-@bp.route('/api/analysis/<path:car>')
+@bp.route('/api/analysis/<path:car>', methods=['GET'])
 def get_car_analysis(car):
     query_params = request.args
     return services.get_car_analysis_service(car, query_params)
 
-# TODO: This route will be used to simulate the scraping process in real time.
-@bp.route('/api/scrape/<car>')
+@bp.route('/api/scrape/<path:car>', methods=['POST'])
 def scrape_reviews(car):
-    return 'Scraping...'
+    year = request.args.get('year')
+    response = asyncio.run(web_scraping.run(car, year))
+    
+    if response:
+        return { 'status': 'success', 'data': response }
+    
+    return { 'status': 'error', 'message': 'Scraped failed' }
